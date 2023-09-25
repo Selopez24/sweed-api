5183afd62d16d088f2f0fccf6dcecf46109ae965
Sebastian Lopez <sebas.lopezm24@gmail.com>
2023-09-25 17:25:55 -0500

    add user not found error when login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
@@ -12,6 +12,9 @@ export class AuthService {
 
   async validateUser(username: string, password: string): Promise<any> {
     const user = await this.usersService.findByUsername(username, true);
+
+    if (!user)
+      throw new HttpException('username was not found', HttpStatus.NOT_FOUND);
 
     const isCorrectPassword = await bcrypt.compare(password, user.password);
 
